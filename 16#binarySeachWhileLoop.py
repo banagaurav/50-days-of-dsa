2edfbbb0b5d9a1189953c51418534c49f60c6c44
banagaurav <gauravbana840@gmail.com>
2026-02-23 13:35:31 +0545

    optimized mid calculate to overcome overflow in worstcase

--- 16#binarySeachWhileLoop.py
+++ 16#binarySeachWhileLoop.py
@@ -6,13 +6,18 @@ end = len(a) - 1
 found = False
 
 while start <= end :
-    mid = (start + end)//2 
+    # mid = (start + end)//2 
+    # above calculation of mid in worst case can be start = max int value and also for end 
+    # we cause overflow and can miscalculate the mid. So, 
+
+    mid = start + ((end-start)//2)
+    
     if target > a[mid]:
         start = mid + 1
     elif target < a[mid]:
         end = mid - 1
     else:  # target == a[mid]
-        print(f"Target found at index: {mid}")
+        print(f"Target:{target} found at index: {mid}")
         found = True
         break
 
